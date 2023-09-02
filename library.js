43878339c216d8df24cd6e98ced2a14ab14b7dc9
Axel <70969800+Zuxulius@users.noreply.github.com>
2023-09-02 15:50:41 +0200

    Basic setup

---
+++ library.js
@@ -0,0 +1,44 @@
+
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+
+    this.info = () => {
+        return `${this.title} by ${this.author}, ${this.pages} pages, read = ${this.read}`;
+    }
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const addBookButton = document.getElementsByClassName("add")[0];
+    const formContainer = document.getElementsByClassName("formContainer")[0];
+
+    addBookButton.addEventListener("click", function() {
+        // Toggle form visibility
+        if (formContainer.style.display === "none") {
+        formContainer.style.display = "block";
+      } else {
+        formContainer.style.display = "none";
+      }
+    })
+
+    const form = document.getElementsByClassName("myForm")[0];
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+        const title = document.getElementById("bookTitle").value;
+        const author = document.getElementById("author").value;
+        const pages = document.getElementById("pages").value;
+        const read = document.getElementById("read").checked;
+        addBookToLibrary(title, author, pages, read);
+        formContainer.style.display = "none";
+    })
+})
