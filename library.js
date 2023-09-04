0a01f5750d4ef39402fe484d259aecd417950e9a
Axel <70969800+Zuxulius@users.noreply.github.com>
2023-09-04 19:02:50 +0200

    implement removal and read status changeability

--- library.js
+++ library.js
@@ -16,14 +16,35 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(title, author, pages, read) {
     const newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
+    // Display with new book
+    displayBooks();
 }
 
+function removeBook(index) {
+    myLibrary.splice(index, 1);
+    displayBooks(); // Re-render the books
+  }
+
 function displayBooks() {
+    // Remove current display
+    while (bookshelf.firstChild) {
+        bookshelf.removeChild(bookshelf.firstChild);
+      }
+    // Create the book and add it to bookshelf
     for (let index = 0; index < myLibrary.length; index++) {
         const book = myLibrary[index];
         const bookDiv = document.createElement("div");
         bookDiv.classList.add("book");
         bookshelf.appendChild(bookDiv);
+        // Add remove button
+        const remove = document.createElement("button");
+        remove.classList.add("remove");
+        remove.innerHTML = "Remove from library"
+        bookDiv.appendChild(remove);
+        remove.addEventListener("click", function() {
+            removeBook(index);
+        })
+        // Add book info
         const bookInfo = document.createElement("div");
         bookInfo.classList.add("info");
         bookDiv.appendChild(bookInfo);
@@ -33,25 +54,39 @@ function displayBooks() {
         const author = document.createElement("h3");
         const pages = document.createElement("p");
         const read = document.createElement("p");
-
+        read.classList.add("read");
+        // Add changeable read status
+        read.addEventListener("click", function() {
+            if (book.read) {
+                book.read = false;
+                read.innerHTML = "Not read";
+                read.style.backgroundColor = "lightcoral";
+            } else {
+                book.read = true;
+                read.innerHTML = "Read"
+                read.style.backgroundColor = "lightgreen";
+        }})
+        // Set info
         bookTitle.innerHTML = book.title;
         by.innerHTML = "by";
         author.innerHTML = book.author;
         pages.innerHTML = book.pages;
         if (book.read) {
-            read.innerHTML = "Read"
+            read.innerHTML = "Read";
+            read.style.backgroundColor = "lightgreen";
         } else {
             read.innerHTML = "Not read"
+            read.style.backgroundColor = "lightcoral";
         }
-
+        // Append to DOM
         bookInfo.append(bookTitle, by, author, pages, read);
-
     }
 }
 
 document.addEventListener("DOMContentLoaded", function() {
     const addBookButton = document.getElementsByClassName("add")[0];
     const formContainer = document.getElementsByClassName("formContainer")[0];
+    formContainer.style.display = "none";
 
     addBookButton.addEventListener("click", function() {
         // Toggle form visibility
